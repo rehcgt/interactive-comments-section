361696c436c7158c0c9cfc6a207ba179c257c095
Raul Hernandez <rehcgt@outlook.com>
2024-07-26 07:17:26 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using interactive_comments_section;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
